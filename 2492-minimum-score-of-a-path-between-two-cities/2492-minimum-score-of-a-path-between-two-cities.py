dc61c1f88249a5581633390cf9a4de660ec2d7ff
Kondameeda Sai babu <144029171+manojsai7@users.noreply.github.com>
2026-07-04 11:52:52 +0530

    Time: 83 ms (99.2%), Space: 51.8 MB (93.64%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,35 +1,29 @@
-from collections import defaultdict, deque
-from typing import List
+class UnionFind:
+    def __init__(self, n):
+        self.parents = list(range(n+1))
+        self.min_weights = [float("inf")] * (n+1)
+
+    def find(self, x):
+        if x != self.parents[x]:
+            self.parents[x] = self.find(self.parents[x])
+        return self.parents[x]
+    
+    def union(self, x, y, weight):
+        x, y = self.find(x), self.find(y)
+        new_weight = min(self.min_weights[x], self.min_weights[y], weight)
+        if x != y:
+            self.parents[x] = y
+            self.min_weights[y] = new_weight
+            return True
+        else:
+            self.min_weights[x] = new_weight
+            return False
+            
 
 class Solution:
-    def minScore(self, n: int, roads: List[List[int]]) -> int:
-        # 1. Build an adjacency list for the graph
-        graph = defaultdict(list)
-        for u, v, distance in roads:
-            graph[u].append((v, distance))
-            graph[v].append((u, distance))
+    def minScore(self, n: int, roads: list[list[int]]) -> int:
+        uf = UnionFind(n)
+        for u, v, w in roads:
+            uf.union(u, v, w)
+        return uf.min_weights[uf.find(1)]
             
-        # 2. Initialize BFS from city 1
-        visited = set()
-        visited.add(1)
-        queue = deque([1])
-        
-        # Track the absolute minimum score encountered
-        min_score = float('inf')
-        
-        # 3. Traverse the connected component
-        while queue:
-            current_city = queue.popleft()
-            
-            for neighbor, distance in graph[current_city]:
-                # Update the minimum score regardless of whether the neighbor has been visited
-                # because we need to check every single edge in this component
-                if distance < min_score:
-                    min_score = distance
-                    
-                # If we haven't visited the neighbor, add it to the queue
-                if neighbor not in visited:
-                    visited.add(neighbor)
-                    queue.append(neighbor)
-                    
-        return min_score
