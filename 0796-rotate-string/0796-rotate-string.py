9434b685b135d10766ecd08245df4a7ff29bb256
Kondameeda Sai babu <144029171+manojsai7@users.noreply.github.com>
2026-07-16 06:40:36 +0530

    Time: 0 ms (100%), Space: 19.3 MB (51.92%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.py
+++ 0796-rotate-string/0796-rotate-string.py
@@ -1,13 +1,14 @@
 class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
-        t=list(s)
-        if len(s)!=len(goal):
-            return False
-        for i in range(len(t)):
-            first=t.pop(0) # t.insert(len(t)-1,i)
-            t.append(first)# t.pop(i)
-            if "".join(t)==goal:
-                return True
+        return len(s)==len(goal) and goal in (s+s)
+        # t=list(s)
+        # if len(s)!=len(goal):
+        #     return False
+        # for i in range(len(t)):
+        #     first=t.pop(0) # t.insert(len(t)-1,i)
+        #     t.append(first)# t.pop(i)
+        #     if "".join(t)==goal:
+        #         return True
         return False
 
         
