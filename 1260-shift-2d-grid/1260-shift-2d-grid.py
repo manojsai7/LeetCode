009bd9e21fa4d2356e4319e8bb3257eb22b473ca
Kondameeda Sai babu <144029171+manojsai7@users.noreply.github.com>
2026-07-20 12:44:57 +0530

    Time: 7 ms (58.44%), Space: 19.8 MB (19.55%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,5 +1,27 @@
-class Solution(object):
-    def shiftGrid(self, grid, k):
-        l, m, n, k = [num for row in grid for num in row], len(grid), len(grid[0]), k % (len(grid) * len(grid[0]))  # grid to list
-        l = l[-k:] + l[:-k]  # shift k times
-        return [l[i * n: i * n + n] for i in range(m)]  # list to grid
+class Solution:
+    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
+        m = len(grid)
+        n = len(grid[0])
+
+        total = m * n
+        k %= total
+
+        ans = [[0] * n for _ in range(m)]
+
+        for i in range(m):
+            for j in range(n):
+
+                old = i * n + j
+                new = (old + k) % total
+
+                r = new // n
+                c = new % n
+
+                ans[r][c] = grid[i][j]
+
+        return ans
+# class Solution(object):
+#     def shiftGrid(self, grid, k):
+#         l, m, n, k = [num for row in grid for num in row], len(grid), len(grid[0]), k % (len(grid) * len(grid[0]))  # grid to list
+#         l = l[-k:] + l[:-k]  # shift k times
+#         return [l[i * n: i * n + n] for i in range(m)]  # list to grid
