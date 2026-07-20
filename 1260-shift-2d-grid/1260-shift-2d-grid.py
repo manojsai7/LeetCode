198621e597929a7215812d1d3201eedf988f2f60
Kondameeda Sai babu <144029171+manojsai7@users.noreply.github.com>
2026-07-20 12:44:20 +0530

    Time: 3 ms (83.13%), Space: 19.7 MB (48.15%) - LeetHub

--- 1260-shift-2d-grid/1260-shift-2d-grid.py
+++ 1260-shift-2d-grid/1260-shift-2d-grid.py
@@ -1,24 +1,5 @@
-class Solution:
-    def shiftGrid(self, grid: List[List[int]], k: int) -> List[List[int]]:
-        m = len(grid)
-        n = len(grid[0])
-
-        for _ in range(k):
-
-            last = grid[m - 1][n - 1]
-
-            # Shift every element one position right
-            for i in range(m - 1, -1, -1):
-                for j in range(n - 1, -1, -1):
-
-                    if i == 0 and j == 0:
-                        continue
-
-                    if j == 0:
-                        grid[i][j] = grid[i - 1][n - 1]
-                    else:
-                        grid[i][j] = grid[i][j - 1]
-
-            grid[0][0] = last
-
-        return grid
+class Solution(object):
+    def shiftGrid(self, grid, k):
+        l, m, n, k = [num for row in grid for num in row], len(grid), len(grid[0]), k % (len(grid) * len(grid[0]))  # grid to list
+        l = l[-k:] + l[:-k]  # shift k times
+        return [l[i * n: i * n + n] for i in range(m)]  # list to grid
