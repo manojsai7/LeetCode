f8d7050207a1f0aa503ac32dcc32896507007634
Kondameeda Sai babu <144029171+manojsai7@users.noreply.github.com>
2026-08-06 19:28:20 +0530

    Time: 3 ms (91.3%), Space: 19.3 MB (46.97%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,5 +1,20 @@
+
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        mapping={}
+        used=set()
+        for i in range(len(s)):
+            if s[i] in mapping:
+                if mapping[s[i]]!=t[i]:
+                    return False
+            else:
+                if t[i] in used:
+                    return False
+                else:
+                    mapping[s[i]]=t[i]
+                    used.add(t[i])
+        return True
+"""
         if len(s)!=len(t):
             return False
         mapST={}
@@ -12,3 +27,4 @@ class Solution:
             mapST[chS]=chT
             mapTS[chT]=chS
         return True
+"""
